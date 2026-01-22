d7d58669811335b4712b475105cd7f546879ccf5
adrianaramirez-hash <adriana.ramirez@udlondres.com>
2026-01-22 10:03:27 -0600

    Update catalogos.py
    Connect catalogos.py to CAT_CARRERAS in Google Sheets

--- catalogos.py
+++ catalogos.py
@@ -1,4 +1,10 @@
 # ============================================================
+# Google Sheet – Catálogo de Carreras
+# ============================================================
+
+CAT_CARRERAS_SHEET_URL = "https://docs.google.com/spreadsheets/d/1CK7nphUH9YS2JqSWRhrgamYoQdgJCsn5tERA-WnwXes/edit?gid=2108194656#gid=2108194656"
+CAT_CARRERAS_SHEET_NAME = "CAT_CARRERAS"
+# ============================================================
 # CATÁLOGOS MAESTROS
 # Dirección Académica UDL
 # ============================================================
